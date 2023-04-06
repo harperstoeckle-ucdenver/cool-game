d3904fc20cbb20a5df3ee444bde230bd9fa25274
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-06 14:28:58 -0600

    Make symbol_to_utf8 return string instead of using output iterators

--- src/pc/main.cpp
+++ src/pc/main.cpp
@@ -3,34 +3,37 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/*
- * Write the utf-8 for a symbol into the range starting at o. Return the number of bytes that were
- * written.
- */
-template <typename O>
-size_t symbol_to_utf8(Symbol s, O o)
+#include <etl/string.h>
+
+using Utf8Char = etl::string<5>;
+
+// Return a string with the utf-8 representation of the symbol.
+auto symbol_to_utf8(Symbol s) -> Utf8Char
 {
 	if (!s.is_block)
 	{
-		*o++ = static_cast<char>(s.value);
-		return 1;
+		return Utf8Char(1, static_cast<char>(s.value));
 	}
 
+	Utf8Char c;
+
 	// Insert the corresponding braille character.
-	*o++ = static_cast<char>(0b11100010);
-	*o++ = static_cast<char>(0b10100000);
-	*o++ = static_cast<char>(
+	c.push_back(static_cast<char>(0b11100010));
+	c.push_back(static_cast<char>(0b10100000));
+	c.push_back(static_cast<char>(
 		0b10000000
 		| (((s.value >> 5) & 1) << 0)
 		| (((s.value >> 3) & 1) << 1)
 		| (((s.value >> 1) & 1) << 2)
 		| (((s.value >> 4) & 1) << 3)
 		| (((s.value >> 2) & 1) << 4)
-		| (((s.value >> 0) & 1) << 5));
-	return 3;
+		| (((s.value >> 0) & 1) << 5)));
+
+	return c;
 }
 
 auto main() -> int
 {
+	puts(symbol_to_utf8({1, 0b100110}).c_str());
 	return 0;
 }
