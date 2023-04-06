64c5aa1fa510291f48610a22b0dcd75c2582a74d
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-06 15:06:10 -0600

    Rework symbol type a bit

--- src/pc/main.cpp
+++ src/pc/main.cpp
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#include <etl/flat_map.h>
 #include <etl/string.h>
 
 using Utf8Char = etl::string<5>;
@@ -10,9 +11,16 @@ using Utf8Char = etl::string<5>;
 // Return a string with the utf-8 representation of the symbol.
 auto symbol_to_utf8(Symbol s) -> Utf8Char
 {
+	// Map a type to a symbol.
+	static etl::flat_map<SymbolType, char, 16> type_map = {
+		{SymbolType::flip_left_right, '|'},
+		{SymbolType::flip_up_down, '-'}
+	};
+
 	if (!s.is_block)
 	{
-		return Utf8Char(1, static_cast<char>(s.value));
+		auto const it = type_map.find(s.s.type);
+		return Utf8Char(1, it == type_map.end() ? '?' : it->second);
 	}
 
 	Utf8Char c;
@@ -22,18 +30,18 @@ auto symbol_to_utf8(Symbol s) -> Utf8Char
 	c.push_back(static_cast<char>(0b10100000));
 	c.push_back(static_cast<char>(
 		0b10000000
-		| (((s.value >> 5) & 1) << 0)
-		| (((s.value >> 3) & 1) << 1)
-		| (((s.value >> 1) & 1) << 2)
-		| (((s.value >> 4) & 1) << 3)
-		| (((s.value >> 2) & 1) << 4)
-		| (((s.value >> 0) & 1) << 5)));
+		| (((s.b.bits >> 5) & 1) << 0)
+		| (((s.b.bits >> 3) & 1) << 1)
+		| (((s.b.bits >> 1) & 1) << 2)
+		| (((s.b.bits >> 4) & 1) << 3)
+		| (((s.b.bits >> 2) & 1) << 4)
+		| (((s.b.bits >> 0) & 1) << 5)));
 
 	return c;
 }
 
 auto main() -> int
 {
-	puts(symbol_to_utf8({1, 0b100110}).c_str());
+	puts(symbol_to_utf8({.b = {1, 0, 0b110010}}).c_str());
 	return 0;
 }
--- src/symbol.hpp
+++ src/symbol.hpp
@@ -3,10 +3,33 @@
 
 #include <stdint.h>
 
-struct Symbol
+enum struct SymbolType : uint8_t
+{
+	flip_up_down,
+	flip_left_right,
+};
+
+struct SpecialSymbol
 {
 	uint8_t is_block : 1;
-	uint8_t value : 7;
+	uint8_t is_locked : 1;
+	uint8_t left : 1;
+	uint8_t right : 1;
+	SymbolType type : 4;
+};
+
+struct BlockSymbol
+{
+	uint8_t is_block : 1;
+	uint8_t is_locked : 1;
+	uint8_t bits : 6;
+};
+
+union Symbol
+{
+	uint8_t is_block : 1;
+	SpecialSymbol s;
+	BlockSymbol b;
 };
 static_assert(sizeof(Symbol) == 1);
 
