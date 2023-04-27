13d7159f8fc5f1e481319aaa63ae05b97cffbce2
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-27 13:31:56 -0600

    Add header for sounds

---
+++ src/sounds.hpp
@@ -0,0 +1,98 @@
+// Note values and melodies.
+#ifndef PITCHES_HPP_INCLUDED
+#define PITCHES_HPP_INCLUDED
+
+namespace note
+{
+inline constexpr unsigned b0 = 31;
+inline constexpr unsigned c1 = 33;
+inline constexpr unsigned cs1 = 35;
+inline constexpr unsigned d1 = 37;
+inline constexpr unsigned ds1 = 39;
+inline constexpr unsigned e1 = 41;
+inline constexpr unsigned f1 = 44;
+inline constexpr unsigned fs1 = 46;
+inline constexpr unsigned g1 = 49;
+inline constexpr unsigned gs1 = 52;
+inline constexpr unsigned a1 = 55;
+inline constexpr unsigned as1 = 58;
+inline constexpr unsigned b1 = 62;
+inline constexpr unsigned c2 = 65;
+inline constexpr unsigned cs2 = 69;
+inline constexpr unsigned d2 = 73;
+inline constexpr unsigned ds2 = 78;
+inline constexpr unsigned e2 = 82;
+inline constexpr unsigned f2 = 87;
+inline constexpr unsigned fs2 = 93;
+inline constexpr unsigned g2 = 98;
+inline constexpr unsigned gs2 = 104;
+inline constexpr unsigned a2 = 110;
+inline constexpr unsigned as2 = 117;
+inline constexpr unsigned b2 = 123;
+inline constexpr unsigned c3 = 131;
+inline constexpr unsigned cs3 = 139;
+inline constexpr unsigned d3 = 147;
+inline constexpr unsigned ds3 = 156;
+inline constexpr unsigned e3 = 165;
+inline constexpr unsigned f3 = 175;
+inline constexpr unsigned fs3 = 185;
+inline constexpr unsigned g3 = 196;
+inline constexpr unsigned gs3 = 208;
+inline constexpr unsigned a3 = 220;
+inline constexpr unsigned as3 = 233;
+inline constexpr unsigned b3 = 247;
+inline constexpr unsigned c4 = 262;
+inline constexpr unsigned cs4 = 277;
+inline constexpr unsigned d4 = 294;
+inline constexpr unsigned ds4 = 311;
+inline constexpr unsigned e4 = 330;
+inline constexpr unsigned f4 = 349;
+inline constexpr unsigned fs4 = 370;
+inline constexpr unsigned g4 = 392;
+inline constexpr unsigned gs4 = 415;
+inline constexpr unsigned a4 = 440;
+inline constexpr unsigned as4 = 466;
+inline constexpr unsigned b4 = 494;
+inline constexpr unsigned c5 = 523;
+inline constexpr unsigned cs5 = 554;
+inline constexpr unsigned d5 = 587;
+inline constexpr unsigned ds5 = 622;
+inline constexpr unsigned e5 = 659;
+inline constexpr unsigned f5 = 698;
+inline constexpr unsigned fs5 = 740;
+inline constexpr unsigned g5 = 784;
+inline constexpr unsigned gs5 = 831;
+inline constexpr unsigned a5 = 880;
+inline constexpr unsigned as5 = 932;
+inline constexpr unsigned b5 = 988;
+inline constexpr unsigned c6 = 1047;
+inline constexpr unsigned cs6 = 1109;
+inline constexpr unsigned d6 = 1175;
+inline constexpr unsigned ds6 = 1245;
+inline constexpr unsigned e6 = 1319;
+inline constexpr unsigned f6 = 1397;
+inline constexpr unsigned fs6 = 1480;
+inline constexpr unsigned g6 = 1568;
+inline constexpr unsigned gs6 = 1661;
+inline constexpr unsigned a6 = 1760;
+inline constexpr unsigned as6 = 1865;
+inline constexpr unsigned b6 = 1976;
+inline constexpr unsigned c7 = 2093;
+inline constexpr unsigned cs7 = 2217;
+inline constexpr unsigned d7 = 2349;
+inline constexpr unsigned ds7 = 2489;
+inline constexpr unsigned e7 = 2637;
+inline constexpr unsigned f7 = 2794;
+inline constexpr unsigned fs7 = 2960;
+inline constexpr unsigned g7 = 3136;
+inline constexpr unsigned gs7 = 3322;
+inline constexpr unsigned a7 = 3520;
+inline constexpr unsigned as7 = 3729;
+inline constexpr unsigned b7 = 3951;
+inline constexpr unsigned c8 = 4186;
+inline constexpr unsigned cs8 = 4435;
+inline constexpr unsigned d8 = 4699;
+inline constexpr unsigned ds8 = 4978;
+} // namespace notes
+
+#endif
