a54804c08ca4a188d72dc57573c401fc741f16a7
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-28 15:48:37 -0600

    Add basic lcd display capability stuff

--- src/arduino/main.cpp
+++ src/arduino/main.cpp
@@ -1,9 +1,21 @@
+// vim: ft=arduino
+
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(2, 3, 4, 7, 8, 9, 10);
 
 void setup()
 {
+	lcd.begin(16, 2);
+	lcd.print("hello world");
+	lcd.cursor();
 }
 
 void loop()
 {
+	static int c = 0;
+	lcd.setCursor(c % 16, c / 16);
+	delay(500);
+	c = (c + 1) % 32;
 }
