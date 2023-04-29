2fb87f312c55c5131ac34733e32f29b09ab39aab
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-29 11:58:27 -0600

    Add basic input handling with the joystick

--- src/arduino/main.cpp
+++ src/arduino/main.cpp
@@ -6,27 +6,68 @@
 
 Game game;
 
+// Pins for the joystick.
+constexpr uint8_t joystick_x_pin = PIN_A0;
+constexpr uint8_t joystick_y_pin = PIN_A1;
+constexpr uint8_t joystick_button_pin = PIN_A2;
+
+// Update the state of a button and return true if it was just pressed.
+auto update_button_state(bool& state, bool new_state) -> bool
+{
+	bool const just_pressed = !state & new_state;
+	state = new_state;
+	return just_pressed;
+}
+
 void setup()
 {
 	Serial.begin(9600);
+
+	pinMode(joystick_x_pin, INPUT);
+	pinMode(joystick_y_pin, INPUT);
+	pinMode(joystick_button_pin, INPUT_PULLUP);
 }
 
 void loop()
 {
-	if (Serial.available() > 0)
-	{
-		InputEvent e;
-		switch (Serial.read())
-		{
-		case 'h': e = InputEvent::move_left; break;
-		case 'l': e = InputEvent::move_right; break;
-		case ' ': e = InputEvent::select; break;
-		case 'r': e = InputEvent::reset; break;
-		case '\t': e = InputEvent::change_mode; break;
-		case 'c': e = InputEvent::check_solution; break;
-		default: return;
-		}
+	// Value in either direction before it's actually considered an input.
+	constexpr int tolerance = 200;
 
-		game.send_input_event(e);
+	static bool left_held = false;
+	static bool right_held = false;
+	static bool up_held = false;
+	static bool down_held = false;
+	static bool select_held = false;
+
+	int const x = analogRead(joystick_x_pin);
+	int const y = analogRead(joystick_y_pin);
+
+	// Check for new button presses.
+	bool const left_pressed = update_button_state(right_held, x < 512 - tolerance);
+	bool const right_pressed = update_button_state(left_held, x > 512 + tolerance);
+	bool const up_pressed = update_button_state(up_held, y < 512 - tolerance);
+	bool const down_pressed = update_button_state(down_held, y > 512 + tolerance);
+	bool const select_pressed =
+		update_button_state(select_held, digitalRead(joystick_button_pin) == LOW);
+
+	if (right_pressed)
+	{
+		game.send_input_event(InputEvent::move_right);
+	}
+	else if (left_pressed)
+	{
+		game.send_input_event(InputEvent::move_left);
+	}
+	else if (up_pressed)
+	{
+		game.send_input_event(InputEvent::check_solution);
+	}
+	else if (down_pressed)
+	{
+		game.send_input_event(InputEvent::change_mode);
+	}
+	else if (select_pressed)
+	{
+		game.send_input_event(InputEvent::select);
 	}
 }
