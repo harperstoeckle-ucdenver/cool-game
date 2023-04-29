33d3bb8c8fea28a6f2d3efdc5ae7fa41b61aabb4
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-04-29 17:16:03 -0600

    Add cheat button to unlock all levels

--- src/game.cpp
+++ src/game.cpp
@@ -97,7 +97,13 @@ void Game::send_input_event(InputEvent e)
 		break;
 
 	case InputEvent::check_solution:
-		if (!in_level_select_mode)
+		// Cheat to make things easier.
+		if (in_level_select_mode)
+		{
+			play_jingle(Jingle::correct);
+			last_unlocked_level = static_cast<int>(num_levels) - 1;
+		}
+		else
 		{
 			if (is_solved(puzzle))
 			{
