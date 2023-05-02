d7fd96eb38e8ca079e67a5a2a53572d86b43127e
Harper Stoeckle <harper.stoeckle@ucdenver.edu>
2023-05-02 15:41:15 -0600

    Make symbol construction constexpr

--- src/symbol.hpp
+++ src/symbol.hpp
@@ -44,23 +44,45 @@ union Symbol
 static_assert(sizeof(Symbol) == 1);
 
 // Get the block bit at the given row and column.
-auto block_bit(Symbol s, int row, int col) -> bool;
+constexpr
+auto block_bit(Symbol s, int row, int col) -> bool
+{
+	return (s.b.bits >> (5 - 2 * row - col)) & 1;
+}
 
 // Set the block bit at the given row and column.
-void set_block_bit(Symbol& s, int row, int col, bool v);
+constexpr
+void set_block_bit(Symbol& s, int row, int col, bool v)
+{
+	int const offset = 5 - 2 * row - col;
 
+	if (v)
+	{
+		s.b.bits |= (1 << offset);
+	}
+	else
+	{
+		s.b.bits &= ~(1 << offset);
+	}
+}
 // Swap bits (r0, c0) and (r1, c1).
-void swap_block_bits(Symbol& s, int r0, int c0, int r1, int c1);
+constexpr
+void swap_block_bits(Symbol& s, int r0, int c0, int r1, int c1)
+{
+	bool const t = block_bit(s, r0, c0);
+	set_block_bit(s, r0, c0, block_bit(s, r1, c1));
+	set_block_bit(s, r1, c1, t);
+}
 
 // Construct a block symbol.
-inline
+constexpr
 auto make_block(uint8_t bits, bool locked = false) -> Symbol
 {
 	return Symbol{.b = {1, locked, bits}};
 }
 
 // Construct a special symbol.
-inline
+constexpr
 auto make_special(bool left, bool right, SymbolType type, bool locked = false) -> Symbol
 {
 	return Symbol{.s = {0, locked, left, right, type}};
